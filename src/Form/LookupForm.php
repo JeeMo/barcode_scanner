9abeea5cad880d92dd6a467d76f40b98442c2dfb
JeeMo <jee@serendipities.net>
2022-04-29 19:27:21 -0500

    Basic module is working with API.  Needs much refactoring!

--- src/Form/LookupForm.php
+++ src/Form/LookupForm.php
@@ -54,18 +54,75 @@ class LookupForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    //Check for barcode in content.
     $barcode = $form_state->getValue('barcode');
-
-    //Check for barcode in content
+    $str = "";
     //If found, display and ask whether to add or subtract 1 from quantity.
-    //If not found, check barcodelookup.com
-    //If found, display API results and ask whether to add to inventory.
-    //If not found, redirect to 'add content of type item' form.
+    $entities = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties([
+      'type' => 'scanned_item',
+      'field_barcode' => $barcode,
+      ]);
+    if ($entities) {
+      // Found in our system.
+      $str .= "Found in our system.  Add or Subtract?  Also offer option to edit item fields.";
+    } else {
+      $str .= "Not found in local system, checking API.";
+      //If not found, check barcodelookup.com
+      $data = $this->get_data($barcode);
+
+      //If found, display API results and ask whether to add to inventory.
+      $response = array();
+      $response = json_decode($data);
+      if ($response) {
+        $str .= '<strong>Barcode Number:</strong> ' . $response->products[0]->barcode_number . '<br><br>';
+        $str .=  '<strong>Title:</strong> ' . $response->products[0]->title . '<br><br>';
+        $str .= '<strong>Entire Response:</strong><pre>';
+        $str .= print_r($response->products, TRUE);
+        $str .= '</pre>';
+
+        // For now, automatically add it.
+        $node = \Drupal::entityTypeManager()->getStorage('node')->create([
+          'type' => 'scanned_item',
+          'title' => $response->products[0]->title,
+          'field_barcode' => $response->products[0]->barcode_number,
+          'body' => $response->products[0]->description,
+        ]);
+        //$node->save();
+        /* TODO: Don't save it yet, offer choices.*/
+      } else {
+        //If not found, redirect to 'add content of type item' form.
+        $str .= "Nothing found by API";
+      }
+    }
+
+
 
     // Display result.
+    /*
     foreach ($form_state->getValues() as $key => $value) {
       \Drupal::messenger()->addMessage($key . ': ' . ($key === 'text_format'?$value['value']:$value));
     }
+*/
+
+
+    \Drupal::messenger()->addMessage($str);
+
   }
 
+  public function get_data($barcode) {
+
+    $api_key = 'dioqrp8uxkc4scvz1n1isb4ixlp59c';
+    $url = 'https://api.barcodelookup.com/v3/products?barcode=' . $barcode . '&formatted=y&key=' . $api_key;
+
+    $ch = curl_init(); // Use only one cURL connection for multiple queries
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    return $data;
+  }
 }
